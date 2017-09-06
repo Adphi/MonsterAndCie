335533ca9236a19880604c0401563e1b1479d0a2
Adphi <philippe.adrien.nousse@gmail.com>
2017-09-06 17:58:14 +0200

    Added Characters Values Rework UI

--- app/src/main/java/be/heroesofmightandmagic/www/monsterandcie/MainActivity.java
+++ app/src/main/java/be/heroesofmightandmagic/www/monsterandcie/MainActivity.java
@@ -1,7 +1,9 @@
 package be.heroesofmightandmagic.www.monsterandcie;
-
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button);
+
+        final Intent intent = new Intent(this, DetailsActivity.class);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(intent);
+            }
+        });
     }
 }
