cbac6bada9ac5396806913d16237f0340d89785e
Adphi <philippe.adrien.nousse@gmail.com>
2017-09-13 13:38:57 +0200

    Code reorganisation and moved level dependant action into a 'setData' method

--- app/src/main/java/be/heroesofmightandmagic/www/monsterandcie/DetailsActivity.java
+++ app/src/main/java/be/heroesofmightandmagic/www/monsterandcie/DetailsActivity.java
@@ -1,15 +1,9 @@
 package be.heroesofmightandmagic.www.monsterandcie;
 
-import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -17,7 +11,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static be.heroesofmightandmagic.www.monsterandcie.R.id.editTextLevelValue;
+import static be.heroesofmightandmagic.www.monsterandcie.R.id.monsterName;
 
 public class DetailsActivity extends AppCompatActivity {
 
@@ -32,53 +26,42 @@ public class DetailsActivity extends AppCompatActivity {
         // Get the Monster Name from the Extra Data of the Intent
         String monsterNameFormated = getIntent().getStringExtra("monsterName");
 
-        final String monsterName = monsterNameFormated.contains(" ") ? monsterNameFormated.replace(" ", "_").toLowerCase() : monsterNameFormated.toLowerCase();
-
         setTitle(monsterNameFormated);
 
-        ImageView evolEggImageView = (ImageView) findViewById(R.id.evolEgg);
-        ImageView evol1ImageView = (ImageView) findViewById(R.id.evol1);
-        ImageView evol2ImageView = (ImageView) findViewById(R.id.evol2);
-        ImageView evol3ImageView = (ImageView) findViewById(R.id.evol3);
-        final ImageView charLevelImageView = (ImageView) findViewById(R.id.characterLevelImage);
+        // Set the Name
+        TextView monsterNameTextView = (TextView) findViewById(monsterName);
+        monsterNameTextView.setText(monsterNameFormated);
 
-        // Get the textview
-
-        final TextView powerTextView = (TextView) findViewById(R.id.powerLevelLabel);
-        TextView lifeTextView = (TextView) findViewById(R.id.lifeLevelLabel);
-        TextView speedTextView = (TextView) findViewById(R.id.speedLevelLabel);
-        TextView staminaTextView = (TextView) findViewById(R.id.staminaLevelLabel);
-        TextView monsterNameTextView = (TextView) findViewById(R.id.monsterName);
-
-        //CharacterLevlImage
-        Drawable charLevelImage = Utils.getResourceDrawableByString(monsterName + "_egg", getApplicationContext());
-        charLevelImageView.setImageDrawable(charLevelImage);
+        //CharacterLevelImage
+        Drawable levelImage = Utils.getResourceDrawableByString(monsterName + "_egg", getApplicationContext());
+        ImageView levelImageView = (ImageView) findViewById(R.id.characterLevelImage);
+        levelImageView.setImageDrawable(levelImage);
 
         // Egg
         Drawable evolEggImage = Utils.getResourceDrawableByString(monsterName + "_egg", getApplicationContext());
+        ImageView evolEggImageView = (ImageView) findViewById(R.id.evolEgg);
         evolEggImageView.setImageDrawable(evolEggImage);
 
         //Evol1
+        ImageView evol1ImageView = (ImageView) findViewById(R.id.evol1);
         Drawable evol1Image = Utils.getResourceDrawableByString(monsterName + "_evol1", getApplicationContext());
         evol1ImageView.setImageDrawable(evol1Image);
 
         //Evol2
+        ImageView evol2ImageView = (ImageView) findViewById(R.id.evol2);
         Drawable evol2Image = Utils.getResourceDrawableByString(monsterName + "_evol2", getApplicationContext());
         evol2ImageView.setImageDrawable(evol2Image);
 
         //Evol3
         Drawable evol3Image = Utils.getResourceDrawableByString(monsterName + "_evol3", getApplicationContext());
+        ImageView evol3ImageView = (ImageView) findViewById(R.id.evol3);
         evol3ImageView.setImageDrawable(evol3Image);
 
-
         // Get the Element
-
         ImageView elementImageView = (ImageView) findViewById(R.id.elementTypeLogo);
         TextView elementTextView = (TextView) findViewById(R.id.elementTypeLabel);
-
         String elementText = Utils.getResourceStringByString(monsterName + "_element", getApplicationContext());
         elementTextView.setText("Type : " + elementText);
-
 
         String elementRessourceName = elementText;
         elementRessourceName = elementRessourceName.toLowerCase() + "_logo";
@@ -86,12 +69,17 @@ public class DetailsActivity extends AppCompatActivity {
         Drawable elementImage = getResources().getDrawable(elementRessourceId, getTheme());
         elementImageView.setImageDrawable(elementImage);
 
-
-
-        monsterNameTextView.setText(monsterNameFormated);
-
+        // Set Stamina (constant over levels)
         String staminaLevel = Utils.getResourceStringByString(monsterName + "_stamina", getApplicationContext());
+        TextView staminaTextView = (TextView) findViewById(R.id.staminaLevelLabel);
         staminaTextView.setText(staminaLevel);
+
+        // Set Data according to default Level
+        setData(monsterNameFormated, 0);
+    }
+
+    public void setData(String monsterNameFormated, int monsterLevel) {
+        final String monsterName = monsterNameFormated.contains(" ") ? monsterNameFormated.replace(" ", "_").toLowerCase() : monsterNameFormated.toLowerCase();
 
         // Get JSON Data back
         String jsonDataString = Utils.getResourceStringByString(monsterName + "_data", getApplicationContext());
@@ -107,26 +95,28 @@ public class DetailsActivity extends AppCompatActivity {
             // Get Value for the Monster's Level
             String powerValue = powerValues.getString(monsterLevel);
 
+            // Get the text View
+            TextView powerTextView = (TextView) findViewById(R.id.powerLevelLabel);
+
             // Set Value
             powerTextView.setText(powerValue);
 
             // Life
             JSONArray lifeValues = jsonData.getJSONArray("life");
             String lifeValue = lifeValues.getString(monsterLevel);
+            TextView lifeTextView = (TextView) findViewById(R.id.lifeLevelLabel);
             lifeTextView.setText(lifeValue);
 
             // Speed
             JSONArray speedValues = jsonData.getJSONArray("speed");
             String speedValue = speedValues.getString(monsterLevel);
+            TextView speedTextView = (TextView) findViewById(R.id.speedLevelLabel);
             speedTextView.setText(speedValue);
 
         }
         catch (final JSONException e) {
-
+            // Simple debug message
             Log.e("Monsters&Cie", "Json parsing error: " + e.getMessage());
         }
-
-
-
     }
 }
