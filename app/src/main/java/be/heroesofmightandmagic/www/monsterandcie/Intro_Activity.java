9bf21c9dd107ee81c35edc858d3311e6ce99df73
Sautronl <sautronl31@gmail.com>
2017-09-14 16:08:23 +0200

    animation intro

--- app/src/main/java/be/heroesofmightandmagic/www/monsterandcie/Intro_Activity.java
+++ app/src/main/java/be/heroesofmightandmagic/www/monsterandcie/Intro_Activity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Intro_Activity extends AppCompatActivity {
 
@@ -15,6 +18,9 @@ public class Intro_Activity extends AppCompatActivity {
         final Intent intent = new Intent(this, MainActivity.class);
 
         ImageView logo = (ImageView) findViewById(R.id.logo);
+        TextView myImageView = (TextView) findViewById(R.id.press_logo);
+        Animation myFadeInAnimation = AnimationUtils.loadAnimation(Intro_Activity.this, R.anim.touch_screen_anim);
+        myImageView.startAnimation(myFadeInAnimation);
 
         logo.setOnClickListener(new View.OnClickListener() {
             @Override
